5e4a4b5ca21b33828ab037a109ac8d2797da2d09
shawl001 <iamultima0@gmail.com>
2015-11-07 21:18:21 -0500

    Title Screen and Particles

---
+++ Assets/startbutton.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class startbutton : MonoBehaviour {
+
+    public void startbuttonpressed(string levelname)
+    {
+        Application.LoadLevel(levelname);
+    }
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
